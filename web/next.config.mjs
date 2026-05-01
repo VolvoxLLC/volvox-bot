cffe3a24e74d8d79b32e7d6211ee405d3cd9538e
Pip Build <bill@chirico.dev>
2026-05-01 05:45:44 -0400

    fix: use current Sentry config API

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -71,15 +71,10 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(
-  nextConfig,
-  {
-    org: process.env.SENTRY_ORG,
-    project: process.env.SENTRY_PROJECT,
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-    silent: !process.env.CI,
-  },
-  {
-    widenClientFileUpload: Boolean(process.env.SENTRY_AUTH_TOKEN),
-  },
-);
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  silent: !process.env.CI,
+  widenClientFileUpload: Boolean(process.env.SENTRY_AUTH_TOKEN),
+});
